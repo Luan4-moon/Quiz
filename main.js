9fef8aa7b68c6c075843cb78c890fb8b557a137a
Luana-boom <luana.silvamoraess0@gmail.com>
2025-07-27 01:06:33 -0300

    etapa-3
    adicionado páginas de apresentação dos quizes. Irei trocar algumas imagens no futuro e adiciona créditos em todos

--- main.js
+++ main.js
@@ -1,11 +1,9 @@
 const toggleBtn = document.getElementById('toggle-dark');
 
-// Aplicar modo salvo ao carregar a página
 if (localStorage.getItem('dark-mode') === 'true') {
     document.body.classList.add('dark-mode');
     toggleBtn.textContent = '☀️';
 }
-
 toggleBtn.addEventListener('click', () => {
     document.body.classList.toggle('dark-mode');
     const isDark = document.body.classList.contains('dark-mode');
